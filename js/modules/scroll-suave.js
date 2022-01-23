18f77b1fbdd5bc22529949f7b6535bdd26171375
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-01-23 15:28:30 -0300

    ScrollSuave refatorado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,28 +1,32 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-  const menuButton = document.querySelector('[data-menu="button"');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    // Fazer o bind com funções callback
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
 
-  function scrollToSection(event) {
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-
-    menuButton.click();
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //     top: topo,
-    //     behavior: 'smooth',
-    // });
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
